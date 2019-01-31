65ce5d95a1db1e89523abcb9c063ee21d5b2d261
@mauroef <cypress90@gmail.com>
2019-01-30 21:54:25 -0300

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,212 @@
+// ToDo pag 96 code organization and testing
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// query string
+const DEFAULT_QUERY = 'redux';
+const DEFAULT_PAGE = 0;
+const DEFAULT_HPP = '100';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
+
+// function isSearched(searchTerm) {
+//   return function (item) {
+//     // returns True or False
+//     return !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
+//   }  
+// }
+
+// more readable
+// recieves a title, if matches with the item condition returns true and the item stays in the list
+// const isSearched = (searchTerm) => (item) => !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: null,
+      searchKey: '',
+      searchTerm: DEFAULT_QUERY,
+    }
+    
+    this.needsToSearchTopstories = this.needsToSearchTopstories.bind(this);
+    this.setSearchTopstories = this.setSearchTopstories.bind(this);
+    this.fetchSearchTopstories = this.fetchSearchTopstories.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  needsToSearchTopstories(searchTerm) {
+    return !this.state.results[searchTerm];
+  }
+
+  setSearchTopstories(result) {
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+    
+    const oldHits = results && results[searchKey]
+      ? results[searchKey].hits
+      : [];
+
+    const updatedHits = [
+    ...oldHits,
+    ...hits
+    ];
+
+    this.setState({
+      result: {
+        ...results, 
+        [searchKey]: { hits: updatedHits, page }
+      }
+    });
+  }
+  
+  fetchSearchTopstories(searchTerm, page) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+    .then(response => response.json())
+    .then(result => this.setSearchTopstories(result));
+  }
+  
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    // client cache
+    this.setState({ searchKey: searchTerm });
+    this.fetchSearchTopstories(searchTerm, DEFAULT_PAGE);
+  }
+  
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    
+    if(this.needsToSearchTopstories(searchTerm)) {
+      this.fetchSearchTopstories(searchTerm, DEFAULT_PAGE);
+    }
+    
+    event.preventDefault();
+  }
+  
+  onSearchChange (event) {
+    this.setState({ searchTerm: event.target.value });
+  }  
+  
+  onDismiss (id) {
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
+
+    const isNotId = item => item.objectID !== id;
+    const updatedHits = hits.filter(isNotId);
+    
+    this.setState({ 
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
+    });
+  }
+
+
+  render() {    
+    // destructuring below
+    const { 
+      searchTerm, 
+      results,
+      searchKey
+    } = this.state;
+
+    const page = (
+      results && 
+      results[searchKey] &&
+      results[searchKey].page
+    ) || 0;   
+
+    const list = (
+      results &&
+      results[searchKey] &&
+      results[searchKey].hits
+    ) || [];
+
+    return (            
+      <div className="page">                
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
+          > 
+            Search
+          </Search>         
+        </div>
+        <Table 
+          list={list}
+          onDismiss={this.onDismiss}          
+        />     
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopstories(searchKey, page + 1)}>
+            More
+          </Button>
+        </div>
       </div>
     );
   }
 }
+
+// stateless func & arrow func without Body (return only)
+const Search = ({ value, onChange, onSubmit, children }) =>
+  <form onSubmit={onSubmit}>
+    <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+    <button type="submit">
+      {children} 
+    </button>
+  </form>
+  
+// destructuring method of using props => const { value, onChange, children } = props;
+const Table = ({ list, onDismiss }) =>
+  <div className="table">
+    { list.map(item => 
+      <div key={ item.objectID } className="table-row">
+        <span style={{ width: '40%' }}>
+          <a href={ item.url }>{ item.title }</a>
+        </span>
+        <span style={{ width: '30%' }}>
+          { item.author }
+        </span>
+        <span style={{ width: '10%' }}>
+          { item.num_comments }
+        </span>
+        <span style={{ width: '10%' }}>
+          { item.points }
+        </span>
+        <span style={{ width: '10%' }}>
+          <Button          
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+          >
+            Dismiss
+          </Button>
+        </span>
+      </div>
+  )}      
+  </div>
+
+
+const Button = ({ onClick, className = '', children }) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  > 
+    {children}
+  </button>
 
 export default App;
